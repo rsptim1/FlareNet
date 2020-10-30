096a7371f69fa01e2b7ae03bada5def105f10f7b
rsptim1 <rsptim1@gmail.com>
2020-10-30 15:02:49 -0400

    Add a basic console program for testing connections
    This will suffice for now.

---
+++ FlareNetTestingConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using FlareNet;
+
+namespace FlareNetTestingConsole
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// This actually works
+			var server = new FlareServer(34377, null);
+			var client = new FlareClient("127.0.0.1", 34377);
+			Console.ReadKey();
+			client.Disconnect();
+			server.Disconnect();
+		}
+	}
+}

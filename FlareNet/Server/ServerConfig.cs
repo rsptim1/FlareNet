997e9a5b577b124afb282ece883edef2c2d5d27c
rsptim1 <rsptim1@gmail.com>
2021-06-03 23:42:56 -0400

    Add ChannelCount to ServerConfig and increase default connection limit

--- FlareNet/Server/ServerConfig.cs
+++ FlareNet/Server/ServerConfig.cs
@@ -2,9 +2,16 @@
 {
 	public class ServerConfig
 	{
+		internal const int DefaultChannelCount = 1;
+
 		/// <summary>
 		/// The maximum connections a server can have.
 		/// </summary>
-		public int MaxConnections = 16;
+		public int MaxConnections = 32;
+
+		/// <summary>
+		/// The amount of channels available for sending messages.
+		/// </summary>
+		public int ChannelCount = DefaultChannelCount;
 	}
 }
